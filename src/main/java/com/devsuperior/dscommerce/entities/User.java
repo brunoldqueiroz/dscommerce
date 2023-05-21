8f3e96ddda4619e6c56a8de8dace02a1ebc466c2
Bruno Queiroz <brunoldqueiroz@outlook.com>
2023-05-21 17:12:40 -0400

    Add order getter in User class

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -82,4 +82,8 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
 }
