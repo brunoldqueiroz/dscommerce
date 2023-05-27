c2c9e356734233af0bbc4b1cd33e6ae482b06a57
Bruno Queiroz <brunoldqueiroz@outlook.com>
2023-05-27 16:45:15 -0400

    Add bean validation

--- src/main/java/com/devsuperior/dscommerce/dto/CustomError.java
+++ src/main/java/com/devsuperior/dscommerce/dto/CustomError.java
@@ -2,13 +2,13 @@ package com.devsuperior.dscommerce.dto;
 
 import java.time.Instant;
 
-public class CustomDTO {
+public class CustomError {
     private Instant timestamp;
     private Integer status;
     private String error;
     private String path;
 
-    public CustomDTO(Instant timestamp, Integer status, String error, String path) {
+    public CustomError(Instant timestamp, Integer status, String error, String path) {
         this.timestamp = timestamp;
         this.status = status;
         this.error = error;
