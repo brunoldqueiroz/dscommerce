de4922a3f40be2a154effe612947c910f794122e
Bruno Queiroz <brunoldqueiroz@outlook.com>
2023-06-08 17:03:57 -0400

    Fix query in UserRepository and update entity User

--- src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
@@ -11,10 +11,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query(
             nativeQuery = true,
-            value = "SELECT u.email AS username, u.password, r.id AS roleId, r.authority" +
+            value = "SELECT u.email AS username, u.password, r.id AS roleId, r.authority " +
                     "FROM tb_user AS u " +
                     "INNER JOIN tb_user_role AS ur ON ur.user_id = u.id " +
-                    "INNER JOIN tb_roles AS r ON r.id = ur.role_id " +
+                    "INNER JOIN tb_role AS r ON r.id = ur.role_id " +
                     "WHERE u.email = :email"
     )
     List<UserDetailProjection> searchUserAndRolesByEmail(String email);
