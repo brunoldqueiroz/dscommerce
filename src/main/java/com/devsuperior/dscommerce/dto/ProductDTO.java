8f5419abce21d143d92b3d76c0138efe22fb3a85
Bruno Queiroz <brunoldqueiroz@outlook.com>
2023-06-15 18:22:48 -0400

    Fix product validation

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -17,6 +17,7 @@ public class ProductDTO {
     @Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
     private String description;
 
+    @NotNull(message = "Campo requerido")
     @Positive(message = "O preço precisa ser positivo")
     private Double price;
     private String imgUrl;
